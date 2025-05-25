120e706a05aa7e13c45e7289f6d95e54ba13c388
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pokus@gmail.com>
2025-05-25 16:11:20 +0200

    Squashed commit of the following:
    commit d00736f2959af713e7d3bb56fd3ab02f66d92c50
    Author: Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pokus@gmail.com>
    Date:   Sun May 25 16:10:33 2025 +0200
    
        Squashed commit of the following:
    
        commit 39426612812b35369193f89b125ce86dbe865ec8
        Author: Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pokus@gmail.com>
        Date:   Sun May 25 15:46:48 2025 +0200
    
            prepare first release

---
+++ frontend/src/routes/_layout/speakout.tsx
@@ -0,0 +1,23 @@
+// import { useState } from "react";
+import Controller from "./../../components/Speechtotext/Controller";
+// import React from "react";
+import * as React from "react"
+import { createFileRoute } from "@tanstack/react-router"
+
+export const Route = createFileRoute("/_layout/speakout")({
+  component: Speakout,
+})
+
+function Speakout(): React.JSX.Element {
+  //const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <>
+        <Controller />
+      </>
+    </div>
+  );
+}
+
+export default Speakout;
