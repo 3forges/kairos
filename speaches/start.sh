1f748beb441f5534a516b7b711291d8bdd481e33
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pokus@gmail.com>
2025-05-31 13:01:59 +0200

    refine faster whisper tuning

--- speaches/start.sh
+++ speaches/start.sh
@@ -4,4 +4,8 @@ export COMPOSE_FILE=${COMPOSE_FILE:-compose.cpu.yaml}
 # export WHISPER__COMPUTE_TYPE=int8
 export WHISPER__COMPUTE_TYPE=${WHISPER__COMPUTE_TYPE:-'float32'}
 export ALLOW_ORIGINS=${ALLOW_ORIGINS:-'["*"]'}
+export ENABLE_UI=${ENABLE_UI:-'False'}
+export DEFAULT_LANGUAGE=${DEFAULT_LANGUAGE:-'fr'}
+export DEFAULT_RESPONSE_FORMAT=${DEFAULT_RESPONSE_FORMAT:-'json'}
+
 docker-compose up -d --force-recreate
